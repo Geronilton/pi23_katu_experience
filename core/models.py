2640fb8fdb94b3f7f766b5a022c723d5939cb81e
geronilton <geronilton001@gmail.com>
2023-11-11 20:13:36 -0300

    criei CRUD cliente

--- core/models.py
+++ core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Cliente(models.Model):
+    nome = models.CharField("Nome", max_length=100)
+    telefone = models.IntegerField("Telefone", max_length=11)
+    email = models.EmailField("Email", max_length=100)
+    cpf = models.IntegerField("CPF", max_length=11)
