cfe29afca4c5672bb2f61c55d34a888666d3e3d4
J-Jordson <joasjordson19@gmail.com>
2023-11-30 08:09:16 -0300

    Inicio

--- core/models.py
+++ core/models.py
@@ -7,3 +7,14 @@ class Cliente(models.Model):
     telefone = models.IntegerField("Telefone", max_length=11)
     email = models.EmailField("Email", max_length=100)
     cpf = models.IntegerField("CPF", max_length=11)
+
+class Passeio(models.Model):
+    titulo = models.CharField("Titulo", max_length=50)
+    decricao = models.CharField("Descrição", max_length=50)
+    valor = models.IntegerField("Valor")
+
+class Serviços(models.Model):
+    nome = models.CharField("Titulo", max_length=50)
+
+class Participantes(models.Model):
+    nome = models.CharField("Titulo", max_length=50)
